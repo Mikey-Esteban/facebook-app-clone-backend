ec5d818d65b8817adb714ffcaf7244ffa82dfb0f
Mikey Esteban <michaelp.esteban@gmail.com>
2021-02-11 17:25:56 -0500

    set up devise for user log in

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,8 +1,8 @@
 class UserSerializer
   include JSONAPI::Serializer
-  attributes :id, :email, :name, :created_at
+  attributes :id, :email, :name, :jti, :created_at
 
   attribute :created_date do |user|
          user && user.created_at.strftime('%d/%m/%Y')
-  end  
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,10 @@ Rails.application.routes.draw do
     sessions: 'users/sessions',
     registrations: 'users/registrations'
   }
+
+  namespace :api do
+    namespace :v1 do
+      resources :users, param: :email, only: [:create]
+    end
+  end
 end
