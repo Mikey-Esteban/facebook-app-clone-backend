3607f5cdcb5b31be3e818c57beed9c81b11b32c1
Mikey Esteban <michaelp.esteban@gmail.com>
2021-02-12 12:46:41 -0500

    deleted UsersController create method

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,14 +1,3 @@
 class Api::V1::UsersController < ApplicationController
 
-  def create
-    puts "PARAMS: #{params}"
-    user = User.find_by(email: params[:user][:email])
-    puts "hiyoooo im in user#create"
-    puts "user: #{user.name}, #{user.password}, #{user.email}"
-
-    render json: UserSerializer.new(user).serializable_hash.to_json
-  end
-
-
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,6 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :users, param: :email, only: [:create]
     end
   end
 end
