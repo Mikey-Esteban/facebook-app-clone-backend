6c5d896246cfcf268904fac157913bf47e6dca44
Mikey <michaelp.esteban@gmail.com>
2021-02-12 15:10:03 -0500

    seeded database with users, created friend_requests#create controller and route

---
+++ app/controllers/api/v1/friend_requests_controller.rb
@@ -0,0 +1,8 @@
+class Api::V1::FriendRequestsController < ApplicationController
+  before_action :authenticate_user!
+
+  def create
+  end
+
+  
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
 
   devise :database_authenticatable, :registerable,
          :jwt_authenticatable, jwt_revocation_strategy: self
+
+  has_many :sent_friend_requests,
+    foreign_key: :requestor_id,
+    class_name: :FriendRequest
+
+  has_many :received_friend_requests,
+    foreign_key: :receiver_id,
+    class_name: :FriendRequest
 end
