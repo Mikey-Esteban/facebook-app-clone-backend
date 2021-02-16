841c5cf1f4109ba5a9752a37d63601c1ea6b678e
Mikey <michaelp.esteban@gmail.com>
2021-02-16 07:50:11 -0500

    added dependencies to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,15 +6,18 @@ class User < ApplicationRecord
 
   has_many :sent_friend_requests,
     foreign_key: :requestor_id,
-    class_name: :FriendRequest
+    class_name: :FriendRequest,
+    dependent: :destroy
 
   has_many :received_friend_requests,
     foreign_key: :receiver_id,
-    class_name: :FriendRequest
+    class_name: :FriendRequest,
+    dependent: :destroy
 
   has_and_belongs_to_many :friendships,
      class_name: "User",
      join_table:  :friendships,
-     foreign_key: :user_id, 
-     association_foreign_key: :friend_user_id
+     foreign_key: :user_id,
+     association_foreign_key: :friend_user_id,
+     dependent: :destroy
 end
