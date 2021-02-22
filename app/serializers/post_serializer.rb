d2e930236353408e621575e93714226d17003935
Mikey <michaelp.esteban@gmail.com>
2021-02-22 10:16:59 -0500

    added author to Post Serializer

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,4 +1,9 @@
 class PostSerializer
   include JSONAPI::Serializer
-  attributes :text, :user_id, :likes, :comments
+  attributes :text, :user_id, :author, :likes, :comments, :created_at
+
+  attribute :author do |post|
+    author = User.find_by(id: post[:user_id])
+    author.name
+  end
 end
